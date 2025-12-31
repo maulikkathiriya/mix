2ae88489d7b47ae8e50da294a9bc6d097a0a2a7f
Your Name <your-email@example.com>
2025-12-31 18:58:22 +0530

    Initial commit

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const {   addUser,  patchUser,    getUsers,   deleteUser,   getUser, updateUser} = require("../controllers/userController");
+const { 
+    addUser, 
+    patchUser, 
+    getUsers, 
+    deleteUser, 
+    getUser, 
+    updateUser 
+} = require("../controllers/userController");
 
-const validate = require("../Middleware/validate"); // Correct import
-const { addUserValidation } = require("../Validations/userValidation");
+const validate = require("../Middleware/validate");
+const { addUserValidation, updateUserValidation, idValidation } = require("../Validations/userValidation");
 
 // GET all users
 router.get("/getusers", getUsers);
@@ -11,16 +18,16 @@ router.get("/getusers", getUsers);
 // CREATE a single user with validation
 router.post("/adduser", validate(addUserValidation), addUser);
 
-// GET a single user by ID
-router.get("/getuser/:id", getUser);
+// GET a single user by ID with ID validation
+router.get("/getuser/:id", validate(idValidation, "params"), getUser);
 
 // UPDATE full user (PUT) with validation
-router.put("/updateuser/:id", validate(addUserValidation), updateUser);
+router.put("/updateuser/:id", validate(updateUserValidation), updateUser);
 
 // UPDATE partial user (PATCH)
 router.patch("/patchuser/:id", patchUser);
 
-// DELETE a user
-router.delete("/deleteusers/:id", deleteUser);
+// DELETE a user with ID validation
+router.delete("/deleteusers/:id", validate(idValidation, "params"), deleteUser);
 
 module.exports = router;
